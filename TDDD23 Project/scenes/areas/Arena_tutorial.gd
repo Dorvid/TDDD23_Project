637560a49a56e323238cc100030dfe368e1f7a8e
davfr261 <davfr261@student.liu.se>
2021-10-05 16:33:07 +0200

    Restructured tutorial code

--- TDDD23 Project/scenes/areas/Arena_tutorial.gd
+++ TDDD23 Project/scenes/areas/Arena_tutorial.gd
@@ -1,68 +1,103 @@
 extends Node
 
-onready var leave_text = $Leave_text
-onready var bounce_up = $Leave_text/Bounce_up
-onready var enter_text = $Enter_text
-onready var effect_in = $Effect_in
-onready var effect_enter = $Enter_text/Fade_effect
-onready var jump_text = $Jump_text
-onready var effect_jump = $Jump_text/Fade_effect
-
-
+#Enter label
+onready var enter_text = $Labels/Enter_text
+onready var effect_enter = $Labels/Enter_text/Fade_effect
+var faded_in_enter_text = false
+#Jump label
+onready var jump_text = $Labels/Jump_text
+onready var effect_jump = $Labels/Jump_text/Fade_effect
+var faded_in_jump_text = false
+#Attack label
+onready var attack_text = $Labels/Attack_text
+onready var effect_attack = $Labels/Attack_text/Fade_effect
+var faded_in_attack_text = false
+#Leave label
+onready var leave_text = $Labels/Leave_text
+onready var bounce_up = $Labels/Leave_text/Bounce_up
+onready var effect_in = $Labels/Leave_text/Effect_in
+var leave_center
 var bounced_up = true
-var faded_out_enter_text = false
+
+#Shows text
+func mod_in(tween,label,time):
+	tween.interpolate_property(label, 'modulate',Color(1,1,1,0),Color(1,1,1,1),time,Tween.TRANS_CUBIC,Tween.EASE_IN)
+	tween.start()
+
+#Hides text
+func mod_out(tween,label,time):
+	tween.interpolate_property(label, 'modulate',Color(1,1,1,1),Color(1,1,1,0),time,Tween.TRANS_CUBIC,Tween.EASE_IN)
+	tween.start()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if CharacterController.connect("boss_hit",self, "_on_Player_enemy_hit") != OK:
 		print("Failed to connect to boss_hit signal in arena script")
-	effect_enter.interpolate_property(enter_text,'modulate',Color(1,1,1,0),Color(1,1,1,1),2,Tween.TRANS_CUBIC,Tween.EASE_IN)
-	effect_enter.start()
+	mod_in(effect_enter,enter_text,1)
+	leave_center = $Labels/Leave_text.get_global_position().x
 
+func _process(_delta):
+	if faded_in_attack_text == true:
+		if Input.is_action_just_pressed("ui_attack"):
+			mod_out(effect_attack,attack_text,1)
 
 func _on_Player_enemy_hit():
 	$Target_dummy.dummy_hit()
 
 
-func _boss_dead():
-	MusicController.crowd_play()
-	print("Text appering")
-	effect_in.interpolate_property(leave_text,'modulate',Color(1,1,1,0),Color(1,1,1,1),0.5,Tween.TRANS_CUBIC,Tween.EASE_IN)
-	effect_in.start()
-	$Background.play("cheering")
-	$Leave_arena/Leavebox.set_deferred('disabled', false)
-	$Ground_and_walls/Entrance_door/Door.set_deferred('disabled', true)
+#Enter label
+func _enter_fade_completed(_object, _key):
+	if faded_in_enter_text == false:
+		faded_in_enter_text = true
+		$Labels/Start_fight/Start_collision.set_deferred("disabled",false)
+		$Ground_and_walls/Entrance_door/Door.set_deferred("disabled",true)
+	else:
+		mod_in(effect_jump,jump_text,1)
 
 func _on_Start_fight_body_entered(_body):
-	#$Ground_and_walls/Entrance_door/Door.set_deferred('disabled', false)
-	$Start_fight/Start_collision.set_deferred('disabled', true)
-	CharacterController.emit_fight_start()
-	effect_enter.interpolate_property(enter_text,'modulate',Color(1,1,1,1),Color(1,1,1,0),2,Tween.TRANS_CUBIC,Tween.EASE_IN)
-	effect_enter.start()
-	faded_out_enter_text = true
+	$Labels/Start_fight/Start_collision.set_deferred("disabled",true)
+	mod_out(effect_enter,enter_text,1)	
 
+
+#Jump Label
+func _jump_fade_completed(_object, _key):
+	if faded_in_jump_text == false:
+		faded_in_jump_text = true
+		$Labels/Jump_Box/Jumpbox.set_deferred("disabled",false)
+	else:
+		mod_in(effect_attack,attack_text,1)
+
+func _on_Jump_Box_body_entered(_body):
+	$Labels/Jump_Box/Jumpbox.set_deferred("disabled",true)
+	mod_out(effect_jump,jump_text,1)
+
+
+#Attack label
+func _attack_fade_completed(_object, _key):
+	if faded_in_attack_text == false:
+		faded_in_attack_text = true
+	else:
+		mod_in(effect_in,leave_text,1)
+		$Labels/Leave_arena/Leavebox.set_deferred("disabled",false)
+		faded_in_attack_text = false
+
+
+ #Leave label
 func _on_Leave_arena_body_entered(_body):
 	MusicController.leave_arena()
 	$Interface/Life.free_bar_childs()
 	if get_tree().change_scene("res://scenes/areas/Entrance.tscn") != OK:
 		print("Failed to swap to entrance scene")
 
-
 func _on_Effect_in_tween_completed(_object, _key):
-	bounce_up.interpolate_property(leave_text,'rect_position',Vector2(437,108),Vector2(437,100),1,Tween.TRANS_QUINT,Tween.EASE_OUT)
+	bounce_up.interpolate_property(leave_text,'rect_position',Vector2(leave_center,108),Vector2(leave_center,100),1,Tween.TRANS_QUINT,Tween.EASE_OUT)
 	bounce_up.start()
-
 
 func _on_Bounce_up_tween_completed(_object, _key):
 	if bounced_up == false:
 		bounced_up = true
-		bounce_up.interpolate_property(leave_text,'rect_position',Vector2(437,108),Vector2(437,100),0.5,Tween.TRANS_QUINT,Tween.EASE_IN)
+		bounce_up.interpolate_property(leave_text,'rect_position',Vector2(leave_center,108),Vector2(leave_center,100),0.5,Tween.TRANS_QUINT,Tween.EASE_IN)
 	else:
 		bounced_up = false
-		bounce_up.interpolate_property(leave_text,'rect_position',Vector2(437,100),Vector2(437,108),0.5,Tween.TRANS_QUINT,Tween.EASE_IN)
+		bounce_up.interpolate_property(leave_text,'rect_position',Vector2(leave_center,100),Vector2(leave_center,108),0.5,Tween.TRANS_QUINT,Tween.EASE_IN)
 	bounce_up.start()
-
-
-func _Enter_Text_faded(_object, _key):
-	if faded_out_enter_text == true:
-		effect_jump.interpolate_property(jump_text,'modulate',Color(1,1,1,0),Color(1,1,1,1),2,Tween.TRANS_CUBIC,Tween.EASE_IN)
-		effect_jump.start()
